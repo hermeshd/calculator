a1114551ac9b853e76c7017ee16c90fb645e7ff4
hermeshd <hermesd.guerrero@gmail.com>
2024-08-01 12:18:31 -0500

    create basic styling for calculator, next is writing the script

---
+++ script.js
@@ -0,0 +1,32 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+let result = 0;
+
+function add(a, b) {
+
+
+}
+
+function subtract(a, b) {
+
+
+}
+
+
+function multiply(a, b) {
+
+
+}
+
+
+function divide(a, b) {
+
+
+}
+
+
+function operate(operator, a, b) {
+
+
+}
